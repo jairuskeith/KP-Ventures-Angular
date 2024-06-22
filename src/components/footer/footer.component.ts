c021fe65456cf645935d7dfdd6d7d6fe4177de68
Jairus Keith <jairuskeith20@gmail.com>
2024-06-21 22:03:02 -0600

    Updated header.component.html

--- src/components/footer/footer.component.ts
+++ src/components/footer/footer.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatAnchor} from "@angular/material/button";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatAnchor
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
 import {MatIcon} from "@angular/material/icon";
 import {MatIconButton} from "@angular/material/button";
+import {MatToolbar} from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-header',
@@ -11,7 +12,8 @@ import {MatIconButton} from "@angular/material/button";
     MatMenu,
     MatIcon,
     MatIconButton,
-    MatMenuItem
+    MatMenuItem,
+    MatToolbar
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
